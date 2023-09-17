ea13151f3a6f1f67623bd89369f5a1fa2188b578
Camren Mumme <me@typechecked.dev>
2023-09-17 13:20:41 -0400

    Allow spawncommand to resolve with stdout

--- src/util/spawnCommand.ts
+++ src/util/spawnCommand.ts
@@ -1,19 +1,22 @@
 import { spawn } from 'child_process'
 
 export const spawnCommand = (command: string, commandArgs: string[], superuser = false) => {
-    return new Promise<void>((resolve, reject) => {
+    return new Promise<string>((resolve, reject) => {
         const commandProcess = spawn(superuser ? "sudo" : command, superuser ? [ command, ...commandArgs ] : commandArgs);
+        let totalData = ""
 
         commandProcess.stdout.on("data", (data: Buffer) => {
+            totalData += data.toString()
             console.log(data.toString())
         })
         commandProcess.stderr.on("data", (data: Buffer) => {
+            totalData += data.toString()
             console.log(data.toString())
         })
 
         commandProcess.on("close", (exitCode) => {
-            if(exitCode > 0) return reject()
-            return resolve()
+            if(exitCode > 0) return reject(totalData)
+            return resolve(totalData)
         })
     })
 }
